42ada93c4d50fd2b2ffb4532b745964f695050f7
nirmal0001 <kdjj18322@gmail.com>
2025-05-31 22:03:07 +0530

    fix insted of instance use type of for check valid and a prototype for adding books

--- books.js
+++ books.js
@@ -1,4 +1,5 @@
 const myLibrary = new Array();
+const booksDom = document.querySelector('.books');
 
 function Book(title, author, pages, read){
     if (!new.target){
@@ -8,7 +9,7 @@ function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
+    this.read = read ? "✅" : "❎";
     this.info = function(){
         return `${title} by ${author}, ${pages} pages, ${ read ? "read" : "Not read yet"}`;
     };
@@ -16,12 +17,12 @@ function Book(title, author, pages, read){
 
 
 function checkValid(title, author, pages, read) {
-    if (title instanceof String &&
-        author instanceof String &&
-        pages instanceof Number &&
-        read instanceof Boolean){
-            return true;
-        }
+    if (typeof title === "string" &&
+        typeof author === "string" &&
+        typeof pages === "number" &&
+        typeof read === "boolean") {
+        return true;
+    }
     return false;
 
 }
@@ -33,3 +34,18 @@ function addBookToLibrary(title, author, pages, read) {
     let book = new Book(title.trim(), author.trim(), pages, read);
     myLibrary.push(book);
 }
+
+addBookToLibrary("l", "hh", 6768, true);
+addBookToLibrary("l", "hh", 6768, false);
+addBookToLibrary("l", "hh", 6768, true);
+addBookToLibrary("l", "hh", 6768, false);
+
+myLibrary.forEach((item) => {
+    htmlStructure = `<article class="book">
+                        <p class="title">${item.title}</p>
+                        <p><span class="bold">Author : </span> ${item.author}</p>
+                        <p><span class="bold" >Pages : </span> ${item.pages}</p>
+                        <p><span class="bold" >Read : </span> ${item.read}</p>
+                    </article>`;
+    booksDom.insertAdjacentHTML('afterend', htmlStructure);
+})
